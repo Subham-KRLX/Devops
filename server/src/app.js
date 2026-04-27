11c5e0230a3683d9cff2dd047a92015e1e0c5935
Subham Sangwan <subhamsangwan26@gmail.com>
2026-04-27 13:07:56 +0530

    fix: update CORS for AWS deployment

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
